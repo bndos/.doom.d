4e0a194e786daad006597b7aa3c7e0dffd917dce
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2023-06-03 15:54:24 -0400

    don't replace side windows

--- init.el
+++ init.el
@@ -89,7 +89,7 @@
        ;;direnv
        docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
@@ -152,7 +152,8 @@
         +roam2
         +dragndrop
         +pomodoro
-        +jupyter)               ; organize your plain life in plain text
+        ;; +jupyter
+        )               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -172,7 +173,7 @@
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       (swift +lsp)             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +lsp)               ; the tubes
        yaml                ; JSON, but readable
