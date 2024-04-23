0abe1cd3699f9cb2e8fc8bffa8ce3d77c5947a7f
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2024-04-22 22:54:02 -0400

    gptel

--- packages.el
+++ packages.el
@@ -26,6 +26,7 @@
 (package! copilot
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
 (package! leetcode)
+(package! gptel)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
