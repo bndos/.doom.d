e39daa2ccfb3203d740eccca87005226c6ac0744
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2024-04-25 00:19:22 -0400

    workspaces workaround

--- packages.el
+++ packages.el
@@ -27,6 +27,7 @@
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
 (package! leetcode)
 (package! gptel)
+(package! arxiv-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
