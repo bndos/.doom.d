e534f3b114e2be588d2bf12a737ef4d33da396ba
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2024-04-26 17:46:16 -0400

    pulsar

--- packages.el
+++ packages.el
@@ -27,6 +27,7 @@
 (package! leetcode)
 (package! gptel)
 (package! arxiv-mode)
+(package! pulsar)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
