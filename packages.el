336ecd7f312bbb945d79580532d9e3d3c4d77234
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2024-04-26 18:14:55 -0400

    reformat python

--- packages.el
+++ packages.el
@@ -28,6 +28,7 @@
 (package! gptel)
 (package! arxiv-mode)
 (package! pulsar)
+(package! reformatter)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
