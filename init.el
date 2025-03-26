401b6e38e9ca2d99fb9339950f28c807955eb96a
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2025-03-25 22:29:41 -0400

    feat: cc lsp

--- init.el
+++ init.el
@@ -122,10 +122,10 @@
 
        :lang
        ;;agda              ; types of types of types of types...
-       ;; (cc
+       (cc
        ;; +tree-sitter
-       ;; +lsp
-       ;; )                ; C/C++/Obj-C madness
+       +lsp
+       )                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
