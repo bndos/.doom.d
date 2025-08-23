19cdd960ec697128d6ef48fd9091626d438b6216
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2025-08-23 17:16:21 -0400

    feat: fix lsp

--- init.el
+++ init.el
@@ -102,16 +102,18 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       (lsp
-        +eglot)
-       (magit +forge)             ; a git porcelain for Emacs
+       ;; (lsp
+       ;;  +eglot)
+       magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        pass              ; password manager for nerds
        ;; pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
-       (terraform +lsp)         ; infrastructure as code
+       (terraform
+        ;; +lsp
+        )         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;; tree-sitter
        ;;upload            ; map local to remote projects via ssh/ftp
@@ -122,17 +124,19 @@
 
        :lang
        ;;agda              ; types of types of types of types...
-       (cc
+       ;; (cc
        ;; +tree-sitter
-       +lsp
-       )                ; C/C++/Obj-C madness
+       ;; +lsp
+       ;; )                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
-       (dart +flutter +lsp)   ; paint ui and not much else
+       (dart +flutter
+             ;; +lsp
+             )   ; paint ui and not much else
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
@@ -175,7 +179,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python
-        +lsp
+        ;; +lsp
         ;; +tree-sitter
         )           ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
@@ -185,7 +189,7 @@
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust
-        +lsp
+        ;; +lsp
         )              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
